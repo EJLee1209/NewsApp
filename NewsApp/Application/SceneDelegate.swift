59bedc8408d7c4d53c03530a06a674388e116982
EJLee1209 <dldmswo1209@gmail.com>
2024-01-19 17:45:11 +0900

    Create: 프로젝트 폴더 구조 설정

---
+++ NewsApp/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  NewsApp
+//
+//  Created by 이은재 on 1/19/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        
+    }
+
+
+}
+
